29cbb8fcf0db0c55797e1f06405871efc23137dd
tintun123 <mr.tintunoo1997@gmail.com>
2023-05-06 01:18:56 +0630

    first commit

---
+++ src/App.vue
@@ -0,0 +1,386 @@
+<template>
+  <div class="flex items-center justify-start gap-x-2 h-6 p-1 bg-primary-red-01/60 font-bold text-primary">
+      <div class="h-4 w-4">
+        <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="8" cy="8" r="8" fill="#F9C20F"/>
+        </svg>
+      </div>
+      <h1 class="text-secondary-yel-01">FEDERATION OF GENERAL WORKERS MYANMAR</h1>    
+  </div>
+  <div class="relative">
+    <img src="./assets/cover-photo.jpg" class="h-full w-full" alt="">
+    <div class="bg-primary-red-01/60 p-2 w-2/5 min-w-[157px] absolute left-0 bottom-4">
+      <h3 class="text-primary text-white leading-[0.88755rem] font-semibold">
+        Lorem ipsum dolor sit amet consectetur. Odio nisi penatibus nulla integer eu."
+      </h3>
+      <div class="flex items-center justify-center h-full mt-[13px]">
+        <button class="
+      mx-full
+      text-primary
+      font-semibold 
+      text-secondary-yel-01 
+      border 
+      rounded-[11.95rem] 
+      px-[3px]
+      py-1 
+      border-secondary-yel-01
+      active:text-white
+      active:border-primary-red-01/60
+      active:bg-fist/60
+      ">
+      Union Activities
+    </button>
+      </div>
+    </div>
+  </div>
+
+  <div class="mt-4 mx-4 flex gap-x-1 justify-end">
+
+    <div class="bg-secondary-yel-02/60 p-1 rounded-[0.86875rem] shadow-drop-sha">
+      <h3 class="text-primary text-primary-red-01 font-semibold">Advocates</h3>
+    </div>
+
+    <div class="bg-secondary-yel-02/60 p-1 px-2 rounded-[0.86875rem] shadow-drop-sha">
+      <h3 class="text-primary text-primary-red-01 font-semibold">Blogs</h3>
+    </div>
+
+    <div class="bg-secondary-yel-02/60 p-1 rounded-[0.86875rem] shadow-drop-sha">
+      <h3 class="text-primary text-primary-red-01 font-semibold">Our Story</h3>
+    </div>
+
+  </div>
+
+  <div class="mt-6 w-[5.3125rem] flex items-center gap-x-1 bg-primary-red-01 p-1">
+    <div class="w-2 h-2">
+      <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="8" cy="8" r="8" fill="#F9C20F"/>
+      </svg>
+    </div>
+    <h3 class="text-lg text-secondary-yel-02 font-bold">OUR STORY</h3>
+  </div>
+
+  <div class="h-[8.75rem] ml-4 mt-6 flex gap-x-4">
+    <div class="h-full w-full p-2 flex-1 flex items-center bg-secondary-yel-02/20">
+      <p class="text-sm leading-[0.85rem]">
+        “In September 2019, <span class="font-bold text-sm">20 labor unions</span> of various industrial sector established <span class="text-sm font-bold">Federation of General Workers Myanmar</span>, an independent self-sustained organization aim for promoting solidarity among workers&#39; communities and fighting on-going injustices, defending workers &#39; rights and their welfare.”
+      </p>
+    </div>
+
+    <div class="flex-1 relative rounded-l-[10px] shadow-drop-sha" id="story">
+      <div class="flex items-center absolute bottom-3 right-2 px-1 rounded-[11.95px] border border-secondary-yel-02 gap-x-1">
+        <div class="h-2 w-2">
+          <svg viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="4" cy="4" r="3.5" fill="#F9C20F"/>
+            </svg>
+        </div>
+        <h3 class="text-primary text-secondary-yel-02 font-semibold">FGWM</h3>
+      </div>
+    </div>
+
+  </div>  
+
+
+  <div class="mt-6 w-[6.625rem] flex items-center gap-x-1 bg-primary-red-01 p-1">
+    <div class="w-2 h-2">
+      <svg viewBox="0 0 8.5 8.5" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="4" cy="4" r="4" fill="#F9C20F"/>
+      </svg>
+    </div>
+    <h3 class="text-lg text-secondary-yel-02 font-bold">UNION STRIKES</h3>
+  </div>
+
+  <div class="mt-6 ml-4 flex flex-nowrap items-center gap-x-4 overflow-x-scroll scrolling-touch scroll-container">
+    <div class="w-2/3 flex-none relative  h-40 rounded-[10px] shadow-drop-sha">
+      <img src="./assets/labour-protest-07-resize.jpg" class="rounded-[10px] h-full w-full protest-image"  alt="">
+      <div class="content absolute bottom-0 rounded-b-[10px] left-0 px-2 pb-2">
+        <h2 class="text-primary text-white font-bold mb-[-2px]">Protest Title</h2>
+        <div class="date flex items-center mb-1">
+          <span class="date text-x-sm text-white/60 font-bold tracking-tighter">May 21<sup>st</sup></span>
+            <i class="">
+              <svg width="13" height="3" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="8" cy="8" r="8" fill="#ffffff"/>
+              </svg>
+            </i>
+            <span class="text-x-sm text-white/60 font-bold tracking-tighter">2021</span>
+        </div>
+
+        <p class="text-white text-sm leading-[0.725rem] font-medium line-clamp-3 mb-1">
+          Along with 20 labor Unions, FWGM organised a protest participating more than 7000 worker of various sector demanding minimun wages reform and
+        </p>
+
+        <div class="flex items-center">
+          <span class="text-x-sm text-white/60">Photo</span>
+          <svg width="13" height="3" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="8" cy="8" r="8" fill="#ffffff"/>
+          </svg>
+          <span class="text-x-sm text-white/60">
+            Splunch
+          </span>
+        </div>
+
+
+      </div>
+    </div>
+
+    <div class="w-2/3 flex-none relative  h-40 rounded-[10px] shadow-drop-sha">
+      <img src="./assets/labour-protest-03-resize.jpg" class="rounded-[10px] h-full w-full protest-image"  alt="">
+      <div class="content absolute bottom-0 rounded-b-[10px] left-0 px-2 pb-2">
+        <h2 class="text-primary text-white font-bold mb-[-2px]">Protest Title</h2>
+        <div class="date flex items-center mb-1">
+          <span class="date text-x-sm text-white/60 font-bold tracking-tighter">May 21<sup>st</sup></span>
+            <i class="">
+              <svg width="13" height="3" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="8" cy="8" r="8" fill="#ffffff"/>
+              </svg>
+            </i>
+            <span class="text-x-sm text-white/60 font-bold tracking-tighter">2021</span>
+        </div>
+
+        <p class="text-white text-sm leading-[0.725rem] font-medium line-clamp-3 mb-1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime corrupti similique vero, deleniti atque assumenda nihil iusto obcaecati ullam eaque aut aspernatur tenetur?
+        </p>
+
+        <div class="flex items-center">
+          <span class="text-x-sm text-white/60">Photo</span>
+          <svg width="13" height="3" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="8" cy="8" r="8" fill="#ffffff"/>
+          </svg>
+          <span class="text-x-sm text-white/60">
+            Splunch
+          </span>
+        </div>
+
+
+      </div>
+    </div>
+
+    <div class="w-2/3 flex-none relative  h-40 rounded-[10px] shadow-drop-sha">
+      <img src="./assets/labour-protest-08.jpg" class="rounded-[10px] h-full w-full protest-image"  alt="">
+      <div class="content absolute bottom-0 rounded-b-[10px] left-0 px-2 pb-2">
+        <h2 class="text-primary text-white font-bold mb-[-2px]">Protest Title</h2>
+        <div class="date flex items-center mb-1">
+          <span class="date text-x-sm text-white/60 font-bold tracking-tighter">May 21<sup>st</sup></span>
+            <i class="">
+              <svg width="13" height="3" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="8" cy="8" r="8" fill="#ffffff"/>
+              </svg>
+            </i>
+            <span class="text-x-sm text-white/60 font-bold tracking-tighter">2021</span>
+        </div>
+
+        <p class="text-white text-sm leading-[0.725rem] font-medium line-clamp-3 mb-1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime corrupti similique vero, deleniti atque assumenda nihil iusto obcaecati ullam eaque aut aspernatur tenetur?
+        </p>
+
+        <div class="flex items-center">
+          <span class="text-x-sm text-white/60">Photo</span>
+          <svg width="13" height="3" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="8" cy="8" r="8" fill="#ffffff"/>
+          </svg>
+          <span class="text-x-sm text-white/60">
+            Splunch
+          </span>
+        </div>
+
+
+      </div>
+    </div>
+  </div>
+
+  <div class="mt-6 h-[14rem] mr-4 relative rounded-r-[10px]">
+      <img src="./assets/first-protest-february-06-resize.jpg" class="w-full h-full rounded-r-[10px]" alt="">
+      <div id="first-protest" class="absolute rounded-r-[10px] left-0 top-0 bottom-0 flex items-center">
+        <div class="w-5/12 ml-4">
+          <p id="first-protest-para" class="px-2 text-sm relative leading-[0.85rem] font-medium first-letter:text-lg text-white">
+            Amidst the time of apprehension, <span class="font-bold">FWGM</span>    , along with thousand of workers bravely embrace  the uncertainty and took to the street on the 6<sup>th</sup> of February, became the very first protest in the history of <span class="font-bold">Burmese Spring Revolution</span> igniting series of resistant movements later across the country.
+          </p>
+        </div>
+      </div>
+  </div>
+
+  <div class="mt-6 w-[5.375rem] flex items-center gap-x-1 bg-primary-red-01 p-1">
+    <div class="w-2 h-2">
+      <svg viewBox="0 0 8.5 8.5" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="4" cy="4" r="4" fill="#F9C20F"/>
+      </svg>
+    </div>
+    <h3 class="text-lg text-secondary-yel-02 font-bold">OUR VISION</h3>
+  </div>
+
+  <div class="mt-6 relative">
+    <ul class="list-disc pl-4 py-4 relative text-sm text-primary-red-01 bg-secondary-yel-01">
+      <div id="fist" class="float-right">
+        <svg width="201" height="204" viewBox="0 0 201 204" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M119.219 47.173L119.546 46.6548L118.972 46.4386L96.7899 38.0679L116.196 18.3003L139.022 31.7198L110.935 60.3148L119.219 47.173ZM166.669 117.387L167.004 117.579L167.274 117.303L172.178 112.294L166.788 130.957L147.182 151.989L147.048 152.133V152.33V203.485H44.3413L56.8841 141.228L56.9322 140.989L56.7736 140.804L0.66961 75.3214L45.1461 27.331L106.125 50.3433L97.1031 64.6451L77.1821 66.0567L55.3001 57.7136L54.4846 57.4027L54.6289 58.2635L56.8603 71.5753L56.9096 71.8692L57.1915 71.9657L79.329 79.5452L111.413 134.849L111.575 135.13L111.897 135.096L121.113 134.125L121.881 134.044L121.493 133.376L86.8168 73.6243L86.7227 73.4621L86.5451 73.4018L86.3489 73.3352L89.2583 73.1287L166.669 117.387ZM165.127 106.668L142.105 93.1194L176.433 58.1536L199.455 71.6856L165.127 106.668ZM109.828 74.2648L146.376 37.0161L170.591 51.2347L134.042 88.4834L109.828 74.2648ZM58.6714 23.6769L81.2908 0.628946L106.421 15.3997L87.5891 34.5872L58.6714 23.6769Z" fill="#5D5D72" stroke="#5D5D72"/>
+        </svg>
+      </div>
+      <li>
+        FGWM shall facilitate Establishment of labor unions throughout the 
+        Country regardless of race, gender, religion and line of work.
+      </li>
+
+      <li>
+        FWGM shall pursue the viability of fellow Union members.
+      </li>
+
+      <li>
+        FGWM shall work toward better quality of life for workers, ensuring
+      fair and adequate living wage as well as social compensation.
+      </li>
+
+      <li>
+        FGWM shall be committed to ensuring that all workplace are 
+        safe, non-discriminatory, free of sexual harassment and non-violent.
+      </li>
+      <li>
+        FGWM shall continuously work to protect and promote the workers’ 
+        rights and their well-being.
+      </li>
+
+      <li>
+        The voices of worker communities must be involved at making labor 
+        related legislation and government policies
+      </li>
+      <li>
+        Labor exploitation must come to an end.
+      </li>
+      <li>
+        The true value and indispensability of blue-collar workers class must 
+        be bought to light and recognized by the society.
+      </li>
+      <li>
+        Corporate numerous labor advocates across the globe.
+      </li>
+    </ul>
+  </div>
+
+  <div class="mt-6 mx-4 grid grid-cols-2 grid-rows-1 bg-primary-red-5/50 h-[7.9375rem]">
+
+    <div class="w-full h-full flex">
+      <p class="text-primary w-[4.5625rem] m-auto text-white font-medium">
+        Injury to one is 
+        injury to all
+      </p>
+    </div>
+
+
+    <div class="w-full h-full">
+      <div class="mx-4 mt-4">
+        <p class="text-sm mb-4 text-white w-[6.6875rem] mx-auto text-center">
+          We will keep you posted our on-going activities!
+        </p>
+        <div class="form">
+          <form>
+            <input type="mail" name="email" placeholder="email" class="text-sm 
+            text-white 
+            bg-transparent 
+            border 
+            border-secondary-yel-01 
+            rounded-[10.78335px] 
+            px-2 
+            py-1 
+            w-full 
+            h-full
+            placeholder-secondary-yel-01">
+            <button class="
+            text-sm
+            text-white
+            bg-fist/60
+            w-full
+            py-1
+            mb-4
+            rounded-[9.78335rem]" type="submit">Subscribe</button>
+          </form>
+          
+        </div>
+      </div>
+      
+    </div>
+  </div>
+
+
+  <div class="">
+    <div class="flex items-center justify-between gap-x-4 mt-6 px-4 py-6 bg-footer-red">
+      <div class="flex-none">
+        <svg width="64" height="64" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="8" cy="8" r="8" fill="#F9C20F"/>
+        </svg>
+
+      </div>
+      <div class="flex-none">
+        <ul class="text-sm text-white space-y-1">
+          <li class="border-b border-secondary-yel-01">ADVOCATES</li>
+          <li class="border-b border-secondary-yel-01">OUR STORY</li>
+          <li class="border-b border-secondary-yel-01">UNION STRIKES</li>
+          <li class="border-b border-secondary-yel-01">BLOGS</li>
+        </ul>
+      </div>
+      <div class="flex-none">
+        <ul class="text-sm text-white space-y-1">
+          <li class="border-b border-secondary-yel-01">CONTACT</li>
+          <li class="">FWGM.facebook.page</li>
+          <li class="">FWGM.org</li>
+          <li class="">+669-123-456-1</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="w-auto h-[400px]">
+
+  </div>
+
+
+</template>
+
+
+<script setup>
+
+</script>
+
+<style>
+#story {
+  background-image: url(./assets/protest-04-adobe-resize-gradient-add.jpg);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+
+#first-protest {
+  background: rgb(0,0,0);
+  background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.95) 10%, rgba(0,0,0,0.9) 20%, rgba(0,0,0,0.85) 30%, rgba(0,0,0,0.8) 40%, rgba(0,0,0,0.7) 50%, rgba(0,0,0,0.6) 60%, rgba(0,0,0,0.5) 70%, rgba(0,0,0,0.4) 80%, rgba(0,0,0,0.3) 90%, rgba(0,0,0,0.2) 100%);
+}
+.content {
+  background: rgb(0,0,0);
+  background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.95) 10%, rgba(0,0,0,0.9) 20%, rgba(0,0,0,0.85) 30%, rgba(0,0,0,0.8) 40%, rgba(0,0,0,0.7) 50%, rgba(0,0,0,0.6) 60%, rgba(0,0,0,0.5) 70%, rgba(0,0,0,0.4) 80%, rgba(0,0,0,0.3) 90%, rgba(0,0,0,0.2) 100%); 
+}
+
+#first-protest-para::before {
+  content : " ";
+  width: 1rem;
+  height: 1rem;
+  position: absolute;
+  top: -16px;
+  left: -8px;
+  background-image: url(./assets/start-quote.svg);
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+#first-protest-para::after {
+  content : " ";
+  width: 1rem;
+  height: 1rem;
+  position: absolute;
+  bottom: -8px;
+  right: 0px;
+  background-image: url(./assets/end-quote.svg);
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+#fist {
+  clip-path: url(./assets/fist-path.svg);
+  shape-outside: url(./assets/fist-path.svg);
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+const defaultTheme = require('tailwindcss/defaultTheme');
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue, js, ts, jsx, tsx}",
+],
+  theme: {
+    extend: {
+      colors : {
+        'primary-red-01' : '#400303',
+        'primary-red-02' : '#572428',
+        'primary-red-03' : '#2E090A',
+        'primary-red-04' : "#D40B0B",
+        'primary-red-5' : '#290505',
+        'footer-red' : '#400303',
+        'secondary-yel-01' : '#F2F2C2',
+        'secondary-yel-02' : '#F2CAA2',
+        'fist' : '#5D5D72',
+        'logo' : '#F9C20F'
+      },
+      fontSize : {
+        'x-sm' : "0.402rem",
+        'sm' : "0.482rem",
+        'primary' : "0.579rem",
+        'lg' : "0.694rem",
+
+      },
+      fontFamily : {
+        sans : ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow : {
+        'drop-sha' : '2px 2px 4px rgba(64, 3, 3, 0.25)'
+      }
+    },
+  },
+  plugins: [
+  ],
+}
+
